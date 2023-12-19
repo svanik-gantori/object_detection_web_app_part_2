14a746d016e50b76bfea6800862b79152d8d94a7
svanik-gantori <119661803+svanik-gantori@users.noreply.github.com>
2023-12-19 18:28:38 +0530

    Add files via upload

---
+++ ladder.js
@@ -0,0 +1,37 @@
+img="";
+status="";
+
+function preload()
+{
+    img=loadImage('ladder.jpeg');
+}
+
+function setup()
+{
+    canvas=createCanvas(400,380);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function draw()
+{
+    image(img,0,0,400,380);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    console.log(results);
+}
